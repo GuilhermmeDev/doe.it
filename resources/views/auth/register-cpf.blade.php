5773f2bf45065d1324395e1f88513ccc5981d715
GuilhermmeDev <guilherme.dev.morais@gmail.com>
2024-11-20 15:30:05 -0300

    adding style to cpf page with jquery mask

--- resources/views/auth/register-cpf.blade.php
+++ resources/views/auth/register-cpf.blade.php
@@ -4,18 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('layouts.head')
+    <link rel="stylesheet" href="{{asset('css/cpf.css')}}">
     <title>Cadastre seu cpf</title>
 </head>
 <body>
-    <h1>Para continuar, é necessário cadastrar o seu cpf para maior segurança</h1>
-    <div>
-        <form action="/cpf" method="post">
-            @csrf
-            @METHOD('PATCH')
-            <input type="text" name="cpf" id="cpf">
-            <input type="submit" value="Cadastrar">
-        </form>
-    </div>
+    <main class="cadastro-container">
+        <header class="header-logo">Doe.It</header>
+        
+        <section class="cadastro-section">
+            <h1 class="cadastro-title">CPF</h1>
+            <p class="cadastro-subtitle">Insira seu CPF para o cadastro</p>
+            
+            <form class="cadastro-form" method="POST" action="/cpf">
+                @csrf 
+                @METHOD('PATCH')
+                <div class="input-container">
+                    <label for="cpf" class="form-label">CPF</label>
+                    <input type="text" id="cpf" name="cpf" class="input-field" placeholder="Insira seu CPF" required>
+                </div>
+
+                <button type="submit" class="submit-button">Cadastrar</button>
+            </form>
+
+            <div class="separator">
+                <hr class="line left-line">
+                <span class="separator-text">ou</span>
+                <hr class="line right-line">
+            </div>
+
+            <footer class="signup-prompt">
+                <span>Para continuar é necessario seu CPF</span>
+            </footer>
+        </section>
+    </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.16/dist/jquery.mask.min.js"></script>
     <script>
