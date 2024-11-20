4fb4bf8dfc609884f768fcddc4d04a1b2614c00d
GuilhermmeDev <guilherme.dev.morais@gmail.com>
2024-11-20 15:16:57 -0300

    adding style to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    @include('layouts.head')
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+</head>
+<body>
+    <main class="login-container">
+        <header class="header-logo">Doe.It</header>
+        
+        <section class="login-section">
+            <h1 class="login-title">Entrar</h1>
+            <p class="login-subtitle">Você pode entrar com seu e-mail</p>
+            
+            <form class="login-form" method="POST" action="{{route('login')}}">
+                @csrf
+                <div class="input-container">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" id="email" name="email" class="input-field" placeholder="Insira seu e-mail" required>
+                    @error('email')
+                        <p>{{$message}}</p>
+                    @enderror
+                </div>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+                <div class="input-container-senha">
+                    <input type="password" id="password" name="password" placeholder="Insira sua senha" required>
+                    <span class="form-label-senha">Senha</span>
+                    <img id="eyeIcon" src="{{asset('assets/oculto.svg')}}" alt="Mostrar senha" class="eye-icon">
+                    @error('password')
+                        <p>{{$message}}</p>
+                    @enderror
+                  </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                <button type="submit" class="submit-button">Entrar</button>
+            </form>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <div class="separator">
+                <hr class="line left-line">
+                <span class="separator-text">ou</span>
+                <hr class="line right-line">
+            </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <footer class="signup-prompt">
+                <span>Não tem uma conta? </span>
+                <a href="/register" class="signup-link">Cadastre-se</a>
+            </footer>
+        </section>
+    </main>
+</body>
+</html>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+<script>
+    document.getElementById('eyeIcon').addEventListener('click', function () {
+        const passwordField = document.getElementById('password');
+        const eyeIcon = document.getElementById('eyeIcon');
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text'; // Torna o campo visível
+        eyeIcon.src = '{{asset("assets/visivel.svg")}}'; // Altera para o ícone de "visível"
+    } else {
+        passwordField.type = 'password'; // Torna o campo oculto
+        eyeIcon.src = '{{asset("assets/oculto.svg")}}'; // Altera para o ícone de "oculto"
+    }
+    });
+</script>
