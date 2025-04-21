6b29ec72e3252e1397750de7cb346cedf2ddb212
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-04-21 16:46:27 -0300

    changing env variables in the echo.js file

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,14 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: "reverb",
+    key: import.meta.env.REVERB_APP_KEY,
+    wsHost: import.meta.env.REVERB_HOST,
+    wsPort: import.meta.env.REVERB_PORT,
+    wssPort: import.meta.env.REVERB_PORT,
+    forceTLS: (import.meta.env.REVERB_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
 });
