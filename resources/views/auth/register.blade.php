b11db114051559991e1beae7c8052f288b8d326d
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-05-03 18:59:49 -0300

    fix: path in the form and inputs names for DB

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,30 +31,40 @@
         <img src="assets/logo1.svg" alt="Sua imagem aqui" class="w-32 h-32 object-contain">
       </div>
 
-      <form onSubmit="event.preventDefault()" class="space-y-5">
+      <form onSubmit="/register" class="space-y-5" method="POST">
+        @csrf
         <div>
           <label class="font-medium">Nome</label>
           <input
             type="text"
+            name="name"
             required
             class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             placeholder="Insira seu nome"
           />
+          @error('name')
+            @include('layouts.error_popup')
+          @enderror
         </div>
         <div>
           <label class="font-medium">Email</label>
           <input
             type="email"
+            name="email"
             required
             class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             placeholder="Insira seu e-mail"
           />
+          @error('email')
+            @include('layouts.error_popup')
+          @enderror
         </div>
         <div class="relative">
           <label class="font-medium">Senha</label>
           <input
             placeholder="Insira sua senha"
             id="password"
+            name="password"
             type="password"
             required
             class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg pr-10"
@@ -66,6 +76,9 @@
             alt="Mostrar senha"
             class="w-5 h-5 absolute top-10 right-3 cursor-pointer"
           />
+          @error('password')
+            @include('layouts.error_popup')
+          @enderror
         </div>
 
         <div class="relative">
@@ -73,6 +86,7 @@
           <input
             placeholder="Confirme sua senha"
             id="cpassword"
+            name="password_confirmation"
             type="password"
             required
             class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg pr-10"
@@ -84,6 +98,9 @@
             alt="Mostrar senha"
             class="w-5 h-5 absolute top-10 right-3 cursor-pointer"
           />
+          @error('password_confirmation')
+            @include('layouts.error_popup')
+          @enderror
         </div>
 
         <div class="flex justify-center">
@@ -92,6 +109,17 @@
           >
           Cadastrar-se
           </button>
+        </div>
+
+        <!-- Ícone do Google abaixo do botão -->
+        <div class="flex justify-center mt-4">
+          <a class="flex items-center justify-center p-2 border rounded-full hover:bg-gray-50 duration-150 active:bg-gray-100" href="{{ url('/login/google') }}">
+            <img
+              src="https://raw.githubusercontent.com/sidiDev/remote-assets/7cd06bf1d8859c578c2efbfda2c68bd6bedc66d8/google-icon.svg"
+              alt="Google"
+              class="w-6 h-6"
+            />
+          </a>
         </div>
 
         <!-- Link alinhado ao centro -->
