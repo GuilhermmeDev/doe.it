20672ea89596b07e0880215b02927b1d8dc04349
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-06-05 13:08:23 -0300

    fix: some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import flowbitePlugin from 'flowbite/plugin'; // Adicionar para Flowbite
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import flowbitePlugin from "flowbite/plugin"; // Adicionar para Flowbite
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php', // O Laravel usa isso para o JIT em desenvolvimento, pode manter
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js', // Para Vue/React/Alpine ou JS puro
-        './node_modules/flowbite/**/*.js' // Adicionar para Flowbite
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php", // O Laravel usa isso para o JIT em desenvolvimento, pode manter
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js", // Para Vue/React/Alpine ou JS puro
+        "./node_modules/flowbite/**/*.js", // Adicionar para Flowbite'
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [
-        forms,
-        flowbitePlugin 
-    ],
+    plugins: [forms, flowbitePlugin],
 };
