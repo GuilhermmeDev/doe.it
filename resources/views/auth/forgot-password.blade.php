d86135bc64390add994b22ebdf74a775a883803f
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-10-03 21:55:17 -0300

    feat: adding logo to the page title

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('assets/logo1.svg') }}" type="image/x-icon"/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Esqueceu a Senha? - Doeit</title>
     <script>
