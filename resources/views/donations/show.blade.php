5903478658a9df44574b55642a1403fb00948f45
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-10-05 11:07:08 -0300

    feat: changing title

--- resources/views/donations/show.blade.php
+++ resources/views/donations/show.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Code - {{$donation->id}}</title>
+    <title>QR Code - {{$donation->campaign->Title}}</title>
     <link rel="icon" href="{{ asset('assets/logo1.svg') }}" type="image/x-icon"/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
