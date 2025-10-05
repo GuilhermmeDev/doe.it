1a561adc523af18c474d6bb5b00c72ea19965d8d
Gustavo Chaves <gustavosilva712493@gmail.com>
2025-10-04 22:49:10 -0300

    feat: adjusting label, page title, semantic tags and design

--- resources/views/donations/donate.blade.php
+++ resources/views/donations/donate.blade.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Campanha</title>
+    <title>Doação - {{$campaign->Title}}</title>
+    <link rel="icon" href="{{ asset('assets/logo1.svg') }}" type="image/x-icon"/>
     <script defer src="js/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class=" bg-white dark:bg-neutral-900 overflow-x-hidden">
     @include('layouts.secondary_navbar')
 
-  <div class="bg-white dark:bg-neutral-900">
+  <main class="bg-white dark:bg-neutral-900">
     <div class="max-w-screen-xl mx-auto px-4 md:px-8">
-      <div class="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
-        <div class="max-w-lg space-y-3 bg-white dark:bg-neutral-800 rounded-lg p-6">
-          <br><br>
-          <p class="text-[#2AB036] text-3xl font-semibold sm:text-4xl">
+      <div class="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none mb-4">
+        <section class="max-w-lg space-y-3 bg-white dark:bg-neutral-800 rounded-lg p-6 mt-4">
+          <h1 class="text-[#2AB036] text-3xl font-semibold sm:text-4xl">
             Doar para a campanha
-          </p>
-          <p class="font-semibold text-gray-900 dark:text-gray-100">
+          </h1>
+          <p class="font-semibold text-xl text-gray-900 dark:text-gray-100">
             Meta: {{$campaign->meta['target']}}
           </p>
-          <p class="font-semibold text-gray-900 dark:text-gray-100">
+          <p class="font-semibold text-xl text-gray-900 dark:text-gray-100">
             Arrecadado: {{$campaign->meta['current']}}
           </p>
           <br>
@@ -36,12 +36,12 @@
               </template>
             </ul>
           </div>
-        </div>
-        <div class="flex-1 mt-12 sm:max-w-lg lg:max-w-md bg-white dark:bg-neutral-800 rounded-lg p-6">
+        </section>
+        <section class="flex-1 mt-12 sm:max-w-lg lg:max-w-md bg-white dark:bg-neutral-800 rounded-lg p-6">
             <form method="POST" action="/donation/{{$campaign->id}}" class="space-y-8">
             @csrf
             <div class="mb-10 mt-8">
-              <label class="font-medium text-gray-900 dark:text-gray-100"> Quantos kg de comida ou roupa você pretende doar? </label>
+              <label class="font-medium text-gray-900 dark:text-gray-100"> {{$campaign->Type=='food' ? 'Quantos kg de comida você pretende doar?' : 'Quantas unidades de roupa você pretende doar?'}} </label>
               <input
                 type="number"
                 required
@@ -68,10 +68,10 @@
             </button>
             </div>
           </form>
-        </div>
+        </section>
       </div>
     </div>
-  </div>
+  </main>
 
 </body>
 </html>
